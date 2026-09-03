f8abe95c6fc266f53b01dc73bd9426fd015e3830
Haqqi Lucky <haqqiarifin@outlook.com>
2026-09-03 15:26:45 +0700

    tambah dialog manajemensama input

--- Assets/Skrip/NPCRelated/AmbilNamaNPCBuatdiShowOnTrigger.cs
+++ Assets/Skrip/NPCRelated/AmbilNamaNPCBuatdiShowOnTrigger.cs
@@ -1,11 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class AmbilNamaNPCBuatdiShowOnTrigger : MonoBehaviour
 {
+
+    private bool SudahKenal = false;
+    [SerializeField] private TMP_Text teksNama;
+    [SerializeField] private GameObject KotakNama;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        KotakNama.SetActive(false);
     }
 
     // Update is called once per frame
@@ -13,6 +18,30 @@ public class AmbilNamaNPCBuatdiShowOnTrigger : MonoBehaviour
     {
 
     }
-    
-    
+
+    private void KeluarinNama()
+    {
+        KotakNama.SetActive(true);
+        if (SudahKenal == true)
+            teksNama.text =  transform.parent.name;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            KeluarinNama();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        KotakNama.SetActive(false);
+    }
+
+
+    private void MasukDialog()
+    {
+        
+    }
 }
--- Assets/Skrip/PlayerRelated/RayaInputManagement.cs
+++ Assets/Skrip/PlayerRelated/RayaInputManagement.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class RayaWASD : MonoBehaviour
+public class RayaInputManagement : MonoBehaviour
 {
+    [Header("Masalah UI")]
+    [SerializeField] private GameObject UIObjektif;
+    [SerializeField] private GameObject KotakNama;
+    private PlayerInput playerInput;
 
+    [Header("Masalah Player")]
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
@@ -14,6 +19,7 @@ public class RayaWASD : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
@@ -46,4 +52,27 @@ public class RayaWASD : MonoBehaviour
 
     }
 
+    public void DialogSelesai()
+    {
+        playerInput.enabled = true;
+        UIObjektif.SetActive(true);
+    }
+    public void MulaiDialog()
+    {
+        playerInput.enabled = false;
+        Debug.Log("sampe sini1");
+        UIObjektif.SetActive(false);
+        Debug.Log("sampe sini2");
+    }
+
+    public void InteractionWithNPC(InputAction.CallbackContext context)
+    {
+        if (context.performed && KotakNama.activeSelf)
+        {
+            MulaiDialog();
+        }
+    }
+
+    
+    
 }
